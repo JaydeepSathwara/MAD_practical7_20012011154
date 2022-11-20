eb0c8ef1b884b65c7fd93d8e5a7599e8565b3b0c
jaydeep <jaydeepsathwara272@gmail.com>
2022-11-20 19:05:46 +0530

    Initial Commit

---
+++ app/src/main/java/com/jaydeepsathwara/practical_7_154/alarmClockActivity.kt
@@ -0,0 +1,11 @@
+package com.jaydeepsathwara.practical_7_154
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class alarmClockActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_alarm_clock)
+    }
+}
